e1398b92f96a43988dbe1d5d574f747ac68de416
Zuhair <zuhair9747@gmail.com>
2017-05-13 15:28:21 +0530

    GradleFirstLogD
    GradleFirstLogD added

---
+++ GradleTest/firsttestlibrary/src/main/java/com/zuhair/firsttestlibrary/GradleFirstLogD.java
@@ -0,0 +1,17 @@
+package com.zuhair.firsttestlibrary;
+
+import android.util.Log;
+
+/**
+ * Created by zuhair on 13/5/17.
+ */
+
+public class GradleFirstLogD {
+
+    private static String TAG = "ZHR";
+
+    public static void d(String message){
+        Log.d(TAG,message);
+    }
+
+}
